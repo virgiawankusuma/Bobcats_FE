c28ad3ebd70dff91ecf6bc95811ecc1b408b2856
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-04-01 09:03:41 +0700

    Try creating a function that has parameters name of book & quantity of the book, then display sentence information of the product

---
+++ main.js
@@ -0,0 +1,8 @@
+const books = () => {
+    let name = "Atomic Habits";
+    let qty = "10";
+    let sentence = `There are ${qty} ${name} books left in stock`;
+    console.log(sentence);
+}
+
+books();
