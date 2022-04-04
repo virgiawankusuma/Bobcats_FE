deea2291c48ffeb2e4f81480b4c9415c8a0113a2
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-04-04 16:07:38 +0700

    Try creating a function that has parameters name of book & quantity of the book, then display sentence if book can be purchased or not with conditional statement

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const books = (book, qty) => {
     if (qty >= 1) {
         console.log(`There are ${qty} ${book} books left in stock, you can get this book now`);
-    } else if (qty < 1) {
+    } else {
         console.log(`There are ${qty} ${book} books left in stock, you can't get this book now`);
     }
 }
 
-books('Harry Potter', 1);
+books('Harry Potter', 0);
