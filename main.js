854823a69f40b8f4bb96d636050899391ffbdcd2
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-04-05 09:23:45 +0700

    Try creating a function that has parameters name of book & quantity of the book as an object, then display sentence if book can be purchased or not

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
-const books = (book, qty) => {
-    if (qty >= 1) {
-        console.log(`There are ${qty} ${book} books left in stock, you can get this book now`);
-    } else {
-        console.log(`There are ${qty} ${book} books left in stock, you can't get this book now`);
-    }
-}
+// for loop
+// for (let book = 0; book < bookStore.length; book++) {
+//     console.log(bookStore[book]);
+// }
 
-books('Harry Potter', 0);
+// for of loop
+// for (const book of bookStore) {
+//     console.log(book);
+// }
+
+let bookStore = [
+    {
+        name: 'Harry Potter',
+        qty: 1
+    },
+    {
+        name: 'Lord of the Rings',
+        qty: 5
+    },
+    {
+        name: 'The Hobbit',
+        qty: 20
+    },
+    {
+        name: 'The Alchemist',
+        qty: 15
+    }
+];
+
+const buyBook = (name, qty) => {
+    for (const book of bookStore) {
+        if (name === book.name) {
+            if (book.qty > 1) {
+                console.log(`You have successfully purchased ${book.qty} ${book.name} books`)
+                book.qty = book.qty - qty;
+                console.log(`There are ${book.qty} ${book.name} books left in stock, you can get this book anymore now`)
+            } else {
+                console.log(`You have successfully purchased ${book.qty} ${book.name} books`)
+                book.qty = 0;
+                console.log(`There are ${book.qty} ${book.name} books left in stock, you can't get this book anymore now`)
+            }
+        }
+    }
+};
+
+buyBook('Harry Potter', 1);
+// buyBook('The Alchemist', 5);
