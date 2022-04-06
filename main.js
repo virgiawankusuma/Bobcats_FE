2bd2747875bdd6e54a7bc806081e41015298aeae
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-04-06 09:44:42 +0700

    create new project typescript, function with 3 argument and return string 'Typescript'

--- main.js
+++ main.js
@@ -1,47 +1,7 @@
-// for loop
-// for (let book = 0; book < bookStore.length; book++) {
-//     console.log(bookStore[book]);
-// }
-
-// for of loop
-// for (const book of bookStore) {
-//     console.log(book);
-// }
-
-let bookStore = [
-    {
-        name: 'Harry Potter',
-        qty: 1
-    },
-    {
-        name: 'Lord of the Rings',
-        qty: 5
-    },
-    {
-        name: 'The Hobbit',
-        qty: 20
-    },
-    {
-        name: 'The Alchemist',
-        qty: 15
-    }
-];
-
-const buyBook = (name, qty) => {
-    for (const book of bookStore) {
-        if (name === book.name) {
-            if (book.qty > 1) {
-                console.log(`You have successfully purchased ${book.qty} ${book.name} books`)
-                book.qty = book.qty - qty;
-                console.log(`There are ${book.qty} ${book.name} books left in stock, you can get this book anymore now`)
-            } else {
-                console.log(`You have successfully purchased ${book.qty} ${book.name} books`)
-                book.qty = 0;
-                console.log(`There are ${book.qty} ${book.name} books left in stock, you can't get this book anymore now`)
-            }
-        }
-    }
-};
-
-buyBook('Harry Potter', 1);
-// buyBook('The Alchemist', 5);
+var var1 = "Learning Typescript is different than Javascript";
+var var2 = 9;
+var var3 = 19;
+function returnString(var1, var2, var3) {
+    return var1 = "Typescript";
+}
+console.log(returnString(var1, var2, var3));
---
+++ main.ts
@@ -0,0 +1,9 @@
+let var1 :string = "Learning Typescript is different than Javascript";
+let var2 :number = 9;
+let var3 :number = 19;
+
+function returnString(var1:string, var2:number, var3:number){
+    return var1 = "Typescript";
+}
+
+console.log(returnString(var1, var2, var3));
