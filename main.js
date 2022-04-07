cd155ee91b837e6874773f53788146144864b819
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-04-07 16:04:58 +0700

    create funtion array union and custom type

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-var var1 = "Learning Typescript is different than Javascript";
-var var2 = 9;
-var var3 = 19;
-function returnString(var1, var2, var3) {
-    return var1 = "Typescript";
+// object
+var obj = {
+    name: "virgiawan",
+    age: 21
+};
+// array
+var names1 = ["virgiawan", "teguh", "kusuma"];
+var names2 = ["virgiawan", 1, "kusuma"];
+// console.log(names1);
+// tuple
+var student1 = ["virgiawan", "kusuma", 20];
+// Create a function that accepts an array of union type number and string as parameter and return a string of combination of elements in parameters
+function combine(parr) {
+    return parr.join(" ");
 }
-console.log(returnString(var1, var2, var3));
+var case1 = [1, 'data', '3', 'result'];
+var case2 = ['Bejo', 'has', '4', 'sport', 'car'];
+console.log(combine(case1));
+console.log(combine(case2));
+function customCombine(parr) {
+    var result = parr.join(" ");
+    return result;
+}
+console.log(customCombine(case1));
+console.log(customCombine(case2));
--- main.ts
+++ main.ts
@@ -1,9 +1,37 @@
-let var1 :string = "Learning Typescript is different than Javascript";
-let var2 :number = 9;
-let var3 :number = 19;
+// object
+let obj: {name: string, age: number } = {
+    name: "virgiawan",
+    age: 21
+};
 
-function returnString(var1:string, var2:number, var3:number){
-    return var1 = "Typescript";
+// array
+let names1: string[] = ["virgiawan", "teguh", "kusuma"];
+let names2: Array<string|number|string> = ["virgiawan", 1, "kusuma"];
+// console.log(names1);
+
+// tuple
+let student1: [string, string, number] = ["virgiawan", "kusuma", 20];
+
+
+// Create a function that accepts an array of union type number and string as parameter and return a string of combination of elements in parameters
+function combine(parr: (string|number)[]) {
+    return parr.join(" ");
 }
 
-console.log(returnString(var1, var2, var3));
+let case1 = [1, 'data', '3', 'result'];
+let case2 = ['Bejo', 'has', '4', 'sport', 'car'];
+
+console.log(combine(case1));
+console.log(combine(case2));
+
+
+// Create a function that accepts Custom Type of array of string and array of number
+type customType = (string|number)[];
+
+function customCombine (parr: customType) {
+    let result = parr.join(" ");
+    return result;
+}
+
+console.log(customCombine(case1));
+console.log(customCombine(case2));
