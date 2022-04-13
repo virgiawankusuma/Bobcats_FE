d8775810a28c98a0c7599116a04ae843b9649540
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-04-13 10:22:10 +0700

    using encapsulation

--- angular-learning/src/app/lists/list-item/list-item.component.ts
+++ angular-learning/src/app/lists/list-item/list-item.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
   templateUrl: './list-item.component.html',
-  styleUrls: ['./list-item.component.css']
+  styleUrls: ['./list-item.component.css'],
+  encapsulation: ViewEncapsulation.None // ShadowDom // Emulated
 })
 export class ListItemComponent implements OnInit {
   @Input('listElement')
