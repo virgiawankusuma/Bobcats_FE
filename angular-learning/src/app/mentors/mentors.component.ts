988400b3e504d3ac0e79222812e49573fa72b283
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-04-27 14:32:55 +0700

    [Angular Material Day 2] Implement angular material table with filter and progress spinner

--- angular-learning/src/app/mentors/mentors.component.ts
+++ angular-learning/src/app/mentors/mentors.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MentorsService } from './mentors.service';
 
@@ -7,12 +8,15 @@ import { MentorsService } from './mentors.service';
   templateUrl: './mentors.component.html',
   styleUrls: ['./mentors.component.css']
 })
-export class MentorsComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'user_type', 'email', 'user_status'];
+export class MentorsComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = ['no', 'name', 'user_type', 'email', 'user_status'];
 
   dataSource!:MatTableDataSource<any>;
 
   loading = false;
+
+  // @ViewChild(MatPaginator)
+  // paginator!: MatPaginator;
 
   constructor(
     private mentorsService: MentorsService
@@ -32,5 +36,8 @@ export class MentorsComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
   }
 
+  ngAfterViewInit() {
+    // this.paginator.page.pipe().subscribe();
+  }
 
 }
